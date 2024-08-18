9159143d0ccf8f3d8dcd378927b87601fc6054a1
h3000plus <hridoy.3000plus@gmail.com>
2024-08-18 13:45:19 +0600

    feat: added getmonth util funciton and context wrapper with all necessary states and local storage

---
+++ src/context/ContextWrapper.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useReducer, useMemo } from "react";
+import GlobalContext from "./GlobalContext";
+import dayjs from "dayjs";
+
+function savedEventsReducer(state, { type, payload }) {
+  switch (type) {
+    case "push":
+      return [...state, payload];
+    case "delete":
+      return state.filter((evt) => evt.id !== payload.id);
+    default:
+      throw new Error();
+  }
+}
+function initEvents() {
+  const storageEvents = localStorage.getItem("events");
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
+  return parsedEvents;
+}
+
+export default function ContextWrapper(props) {
+  const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [daySelected, setDaySelected] = useState(dayjs());
+  const [showEventModal, setShowEventModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [savedEvents, dispatchCalEvent] = useReducer(
+    savedEventsReducer,
+    [],
+    initEvents
+  );
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(savedEvents));
+  }, [savedEvents]);
+
+  useEffect(() => {
+    if (!showEventModal) {
+      setSelectedEvent(null);
+    }
+  }, [showEventModal]);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        monthIndex,
+        setMonthIndex,
+        daySelected,
+        setDaySelected,
+        showEventModal,
+        setShowEventModal,
+        dispatchCalEvent,
+        selectedEvent,
+        setSelectedEvent,
+        savedEvents,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+}
